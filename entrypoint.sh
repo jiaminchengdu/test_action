e1839351e70966366bc5fbf52b9dfdf474aad606
jiamin.fu <jiamin.fu@ansys.com>
2024-05-29 10:18:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/sh -l
 
 # Use INPUT_<INPUT_NAME> to get the value of an input
-GREETING="Hello, $INPUT_WHO_TO_GREET!"
+#GREETING="Hello, $INPUT_WHO_TO_GREET!"
 
 # Use workflow commands to do things like set debug messages
-echo "::notice file=entrypoint.sh,line=7::$GREETING"
+#echo "::notice file=entrypoint.sh,line=7::$GREETING"
 
 # Write outputs to the $GITHUB_OUTPUT file
-echo "time=$(date)" >>"$GITHUB_OUTPUT"
+#echo "time=$(date)" >>"$GITHUB_OUTPUT"
 
-exit 0
+#exit 0
+
+#!/bin/sh -l
+
+echo "hello $1"
+
+echo "time=$(date)" >> $GITHUB_OUTPUT
